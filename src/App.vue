542cd9d5423e8dc0188161e5282ef04da0d00854
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-18 00:15:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Home />
+  </div>
+</template>
+
+<script lang="ts">
+import Home from "./pages/Home.vue";
+export default { components: { Home } };
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="font-black text-primary">Home</div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style>
+</style>
